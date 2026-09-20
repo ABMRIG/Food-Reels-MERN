0ba3fd55687a764edc74843b37adf5579157be91
abm <ayanendu2002abm@gmail.com>
2026-09-21 01:47:57 +0530

    Fix production DB handling

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const dns = require("dns");
 
 // we need to set dns bcs my NodeJS in this machine is facing some issue while trying to connect to DB
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
+// dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const connectDB = async () => {
 
